ecd67096def7dcc083f01cb43bb72566e900c389
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-21 19:37:00 +0530

    fixing serialization issue

--- dog-rescuer/src/main/java/com/behl/rescuer/service/DogDeliveryService.java
+++ dog-rescuer/src/main/java/com/behl/rescuer/service/DogDeliveryService.java
@@ -5,6 +5,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.behl.rescuer.dto.Dog;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.AllArgsConstructor;
 
@@ -15,7 +18,18 @@ public class DogDeliveryService {
 	private final KafkaTemplate<Null, String> kafkaTemplate;
 
 	public void deliver(final Dog dog) {
-		kafkaTemplate.sendDefault(dog.toString()).addCallback(new LogDeliveryService());
+		String message = parseAsString(dog);
+		kafkaTemplate.sendDefault(message).addCallback(new LogDeliveryService());
+	}
+
+	private String parseAsString(final Dog dog) {
+		String message = null;
+		try {
+			message = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(dog);
+		} catch (JsonProcessingException e) {
+			new RuntimeException("Unable to deliver dog " + dog.toString(), e);
+		}
+		return message;
 	}
 
 }
