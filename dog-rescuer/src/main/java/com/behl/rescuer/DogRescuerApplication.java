0613b9de76025b63ed44c6eb33a44e4f96aade6a
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-21 17:07:47 +0530

    init dog-rescuer application

---
+++ dog-rescuer/src/main/java/com/behl/rescuer/DogRescuerApplication.java
@@ -0,0 +1,13 @@
+package com.behl.rescuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRescuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogRescuerApplication.class, args);
+	}
+
+}
